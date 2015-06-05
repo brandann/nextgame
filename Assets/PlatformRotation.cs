b2b7d11980fbe082e460198c004f471b17e59986
Brandan Haertel <foilpudding@gmail.com>
2015-06-05 13:01:51 -0700

    made platform rotate cool

---
+++ Assets/PlatformRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformRotation : MonoBehaviour {
+	
+	public float dir;
+	private float speed;
+	public float X, Y, Z;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(100,150);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate((new Vector3(X,Y,Z)), dir * (speed * Time.deltaTime));
+		
+	}
+}
