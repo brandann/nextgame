f68f2f9c213596dd995097848b8a9b1938380b30
Brandan Haertel <foilpudding@gmail.com>
2015-06-21 10:05:27 -0700

    started gravity prototype - rotates around sphere but does not turn yet

---
+++ Assets/Gravity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.rotation = Quaternion.LookRotation(Vector3.zero);
+	}
+	
+	void LateUpdate()
+	{
+		//find the vector pointing from our position to the target
+		//var _direction = (new Vector3(0,0,0) - transform.position).normalized;
+		//transform.forward = _direction;
+		//var dir = (new Vector3(0,0,0) - (transform.position * Vector3.forward));
+		
+		// this turns the forward vector to 0,0,0
+		//transform.LookAt(new Vector3(0,0,0));
+		
+		var direction = (new Vector3(0,0,0) - transform.position).normalized;
+		transform.up = direction;
+		transform.position = direction * -1 * 50;
+	}
+}
---
+++ Assets/LocalMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocalMovement : MonoBehaviour {
+
+	public float speed;
+	public float rotateSpeed;
+	public bool Gravity;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetAxis("Horizontal") != 0) // rotational
+		{
+			transform.Rotate((-1 * Vector3.up), -1f * Input.GetAxis("Horizontal") * (rotateSpeed * Time.deltaTime));
+		}
+		if(Input.GetAxis("Vertical") != 0) // movement
+		{
+			transform.position += speed * Time.deltaTime * (Input.GetAxis ("Vertical")) * (this.transform.forward);
+		}
+	}
+	
+	void LateUpdate()
+	{
+		if(Gravity)
+		{
+			var direction = (new Vector3(0,0,0) - transform.position).normalized;
+			transform.up = direction;
+			transform.position = direction * -1 * 50;
+		}
+	}
+}
