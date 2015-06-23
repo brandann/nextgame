230f4fcf9fe90193146aae2864ae91cfc8ea64fa
Brandan Haertel <foilpudding@gmail.com>
2015-06-22 17:54:34 -0700

    added altitude

--- Assets/InitalAlign.cs
+++ Assets/InitalAlign.cs
@@ -6,8 +6,8 @@ public class InitalAlign : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		var direction = (new Vector3(0,0,0) - transform.position).normalized;
-		transform.up = direction;
-		transform.position = direction * 50;
+		transform.up =  -1 * direction;
+		transform.position = direction * -1 * 50;
 	}
 	
 	// Update is called once per frame
--- Assets/LocalMovement.cs
+++ Assets/LocalMovement.cs
@@ -5,50 +5,64 @@ public class LocalMovement : MonoBehaviour {
 
 	public float speed;
 	public float rotateSpeed;
-	public bool Gravity;
 	private Vector3 rotationAxis;
-	private float rotationAngle;
+	public float rotationAngle;
 	private Vector3 position;
 	private Vector3 updirection;
+	private float altitude;
+	private Vector3 origin;
+	
 	
 	// Use this for initialization
 	void Start () {
 		rotationAxis = -1 * Vector3.up;
+		altitude = 55;
 		rotationAngle = 0;
 		position = Vector3.up * -1 * 50;
-		updirection = Vector3.up;
+		updirection = (origin - position).normalized * altitude;
+		origin = new Vector3(0,0,0);
 	}
 	
 	// Update is called once per frame
 	void Update () 
-	{
-	
-		//Vector3 gravityDirection = (transform.position - new Vector3(0,0,0)).normalized;
-		//GetComponent<Rigidbody>().AddForce(Vector3.forward * -1, ForceMode.Acceleration);
+	{	
+		//check for altitude changes
+		if(Input.GetKey(KeyCode.Z))
+		{
+			altitude += .1f;
+			updirection = (origin - position).normalized * altitude;
+		}
+		else if(Input.GetKey(KeyCode.X))
+		{
+			altitude -= .1f;
+			updirection = (origin - position).normalized * altitude;
+		}
 		
+		// check for rotational changes
 		if(Input.GetAxis("Horizontal") != 0) // rotational
 		{
-			rotationAngle += -1f * Input.GetAxis("Horizontal") * (rotateSpeed * Time.deltaTime);
+			rotationAngle += Input.GetAxis("Horizontal") * (rotateSpeed * Time.deltaTime);
 		}
+		
+		// check for forward movement
 		if(Input.GetAxis("Vertical") != 0) // movement
 		{
-			position += speed * Time.deltaTime * (Input.GetAxis ("Vertical")) * (this.transform.forward);
-			updirection = (new Vector3(0,0,0) - position).normalized;
+			position += speed * Time.deltaTime * Input.GetAxis("Vertical") * (this.transform.forward);
+			updirection = (origin - position).normalized * altitude;
 		}
+		/*
+		if(Input.GetKey(KeyCode.Space)) // movement
+		{
+			position += speed * Time.deltaTime * 1 * (this.transform.forward);
+			updirection = (origin - position).normalized * altitude;
+		}
+		*/
 	}
 	
 	void LateUpdate()
 	{
-		transform.position += position;
-		transform.position = updirection * -1 * 50;
+		transform.position = updirection;
 		transform.up = updirection;
 		transform.Rotate( rotationAxis, rotationAngle);
-		
-		if(Gravity)
-		{
-			var direction = (new Vector3(0,0,0) - transform.position).normalized;
-			transform.up = direction;
-			transform.position = direction * -1 * 50;
-		}
 	}
 }
