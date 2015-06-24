c7d23ed7d0be34e89739d3969d5bfcb4e8d5a53c
Brandan Haertel <foilpudding@gmail.com>
2015-06-23 22:36:28 -0700

    cleaned up movement and added gravitational pull

--- Assets/LocalMovement.cs
+++ Assets/LocalMovement.cs
@@ -5,6 +5,8 @@ public class LocalMovement : MonoBehaviour {
 
 	public float speed;
 	public float rotateSpeed;
+	public GameObject sphere;
+	
 	private Vector3 rotationAxis;
 	private float rotationAngle;
 	private Vector3 position;
@@ -12,17 +14,20 @@ public class LocalMovement : MonoBehaviour {
 	private float altitude;
 	private Vector3 origin;
     private bool rotating;
+    private float gravityFactor = .05f;
+    private float altitudeFactor = .2f;
+    private float altitudeMax = 30;
+    private float altitudeMin = 0;
 
-    public GameObject sphere;
 	
 	// Use this for initialization
 	void Start () {
 		rotationAxis = -1 * Vector3.up;
 		altitude = sphere.transform.localScale.x / 2;
 		rotationAngle = 0;
+		origin = new Vector3(0,0,0);
         position = Vector3.up * -1 * (sphere.transform.localScale.x / 2) + Vector3.forward * 60;
 		updirection = (origin - position).normalized * altitude;
-		origin = new Vector3(0,0,0);
         rotating = false;
 	}
 	
@@ -30,45 +35,19 @@ public class LocalMovement : MonoBehaviour {
 	void Update () 
 	{	
 		//check for altitude changes
-		if(Input.GetKey(KeyCode.Z))
-		{
-			altitude += .1f;
-			updirection = (origin - position).normalized * altitude;
-		}
-		else if(Input.GetKey(KeyCode.X))
-		{
-			altitude -= .1f;
-			updirection = (origin - position).normalized * altitude;
-		}
-
-        altitude = Mathf.Clamp(altitude, (sphere.transform.localScale.x / 2), (sphere.transform.localScale.x / 2) + 20);
+		if(Input.GetKey(KeyCode.Z))	{ altitude += altitudeFactor; }
+		if(Input.GetKey(KeyCode.X)) { altitude -= altitudeFactor; }
+		altitude -= gravityFactor;
+		altitude = Mathf.Clamp(altitude, (sphere.transform.localScale.x / 2) + altitudeMin, (sphere.transform.localScale.x / 2) + altitudeMax);
 		
-		// check for rotational changes
-		if(Input.GetAxis("Horizontal") != 0) // rotational
-		{
-			rotationAngle -= Input.GetAxis("Horizontal") * (rotateSpeed * Time.deltaTime);
-		}
+		// check for forward movement and for rotational changes
+		rotationAngle -= Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+		position += speed * Time.deltaTime * -1 * Input.GetAxis("Vertical") * this.transform.forward;
+		updirection = (origin - position).normalized * altitude;
 		
-		// check for forward movement
-		if(Input.GetAxis("Vertical") != 0) // movement
-		{
-			position += speed * Time.deltaTime * -1 * Input.GetAxis("Vertical") * (this.transform.forward);
-            updirection = (origin - position).normalized * altitude;
-		}
-		/*
-		if(Input.GetKey(KeyCode.Space)) // movement
-		{
-			position += speed * Time.deltaTime * 1 * (this.transform.forward);
-			updirection = (origin - position).normalized * altitude;
-		}
-		*/
-	}
-	
-	void LateUpdate()
-	{
-        
-        transform.up = updirection;
-        transform.position = updirection;
-        transform.Rotate(rotationAxis, rotationAngle);
+		// make transform changes
+		transform.up = updirection;
+		transform.position = updirection;
+		transform.Rotate(rotationAxis, rotationAngle);
 	}
 }
