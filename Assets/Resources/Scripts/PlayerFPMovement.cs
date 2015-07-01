0d190f88020bb44473fb28259046e48db4b17671
brandann <foilpudding@gmail.com>
2015-07-01 09:26:51 -0700

    added FP view and control, terrain, oculus SDK

---
+++ Assets/Resources/Scripts/PlayerFPMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFPMovement : MonoBehaviour {
+
+	public float speed;
+	public float rotateSpeed;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate((-1 * Vector3.up), -1f * Input.GetAxis("Horizontal") * (rotateSpeed * Time.deltaTime));
+		transform.position += speed * Time.deltaTime * (Input.GetAxis ("Vertical")) * (this.transform.right);
+	}
+}
